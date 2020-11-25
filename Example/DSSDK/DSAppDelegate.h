a72da95115af15bc71dfdf85748ccb36c58cf9c6
陈翔 <sunlightboom@163.com>
2020-11-25 16:28:37 +0800

    Initial commit

---
+++ Example/DSSDK/DSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DSAppDelegate.h
+//  DSSDK
+//
+//  Created by boomsunlight on 11/25/2020.
+//  Copyright (c) 2020 boomsunlight. All rights reserved.
+//
+
+@import UIKit;
+
+@interface DSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/DSSDK/DSViewController.h
@@ -0,0 +1,13 @@
+//
+//  DSViewController.h
+//  DSSDK
+//
+//  Created by boomsunlight on 11/25/2020.
+//  Copyright (c) 2020 boomsunlight. All rights reserved.
+//
+
+@import UIKit;
+
+@interface DSViewController : UIViewController
+
+@end
